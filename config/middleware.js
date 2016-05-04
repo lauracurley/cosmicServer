ecf26880ea3284ee65e06fb484ec4060d385d2b1
Haoming Huang <Hao@Haomings-MacBook-Pro-2.local>
2016-05-04 15:18:25 -0700

    (feat) Add Morgan middleware to log http requests

--- config/middleware.js
+++ config/middleware.js
@@ -1,9 +1,8 @@
 var bodyParser = require('body-parser');
-
-
+var morgan = require('morgan'); 
 
 module.exports = (app, express) => {
+  app.use(morgan('dev')); //Optional automatic HTTP request/response logger 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-
 };
