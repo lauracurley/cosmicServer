29648001cba4532fe38e2ecb843b25f39b7fdd83
Haoming Huang <Hao@Haomings-MacBook-Pro-2.local>
2016-05-04 15:15:40 -0700

    (cleanup) Cleanup comments in server.js

--- config/index.js
+++ config/index.js
@@ -1,8 +1,7 @@
 module.exports = function() {
-  'use strict';
   var db = require('./db.js');
   var User = require('../models/user.js');
-  var Like = require('../models/like.js');
+  var Interest = require('../models/interest.js');
   var Message = require('../models/message.js');
   var Profile = require('../models/profile.js');
   var User = require('../models/user.js');
