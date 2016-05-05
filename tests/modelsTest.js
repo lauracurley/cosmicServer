87f1bd5db23efff081df74fb58a4526861e8ec55
Haoming Huang <Hao@Haomings-MacBook-Pro-2.local>
2016-05-05 12:29:42 -0700

    (feat) Add first test

---
+++ tests/modelsTest.js
@@ -0,0 +1,10 @@
+var expect = chai.expect;
+// var Fitness = require('./Fitness.js');
+
+describe('Models Test', function() {
+  describe('sub1', function() {
+    it('should return true', function() {
+      expect(true).to.be.true;
+    });
+  });
+});
