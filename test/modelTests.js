d8ef09c65c924f7c0087069252e4343288eb36e5
Haoming Huang <Hao@Haomings-MacBook-Pro-2.local>
2016-05-05 14:26:03 -0700

    (log) Add documentation on how to run tests

--- test/modelTests.js
+++ test/modelTests.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 // var Fitness = require('./Fitness.js');
 
 
-describe('Models Test', function() {
+describe('Model Tests', function() {
   describe('Fitness Model', function() {
     it('should return true', function() {
       expect(true).to.be.true;
