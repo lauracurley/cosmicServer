b6ff682e82374d00b663fa2b6b55e0307ac586cc
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-06 20:34:30 -0700

    Implement function saveFitbit to save fitbitdata to database

--- controllers/fitnessController.js
+++ controllers/fitnessController.js
@@ -1,1 +1,16 @@
 var Fitness = require('../models/fitness.js');
+var user =  require('../models/user.js');
+
+module.exports.saveFitbit = function(fitnessData, facebookID){
+  User.findOne({where: {facebookID: facebookID}}).then(function(user){
+    Fitness.update({steps: fitnessData.steps,
+                    restingHeartRate: fitnessData.restingHeartRate,
+                    sedentaryMinutes: fitnessData.sedentaryMinutes,
+                    lightlyActiveMinutes: fitnessData.lightlyActiveMinutes,
+                    fairlyActiveMinutes: fitnessData.fairlyActiveMinutes,
+                    veryActiveMinutes: fitnessData.veryActiveMinutes
+                    }, {where: {userId: user.id}});
+  });
+};
+
+}
