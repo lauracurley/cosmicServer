e33c607165878cb7cf36ccbf3c2c39c1ed31ebdb
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-09 15:46:58 -0700

    Update saveOne to save profile to a foreign userId

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,26 +1,24 @@
 var Profile = require('../models/profile.js');
+var User = require('../models/user.js');
 
-module.exports.saveOne = function(req, res) {
+module.exports.saveOne = function (req, res) {
+  var facebookId = req.body.facebookId;
   var profileData = {
     age: req.body.age,
     gender: req.body.gender,
     zipCode: req.body.zipCode,
-    picturePath: req.body.picturePath
+    picturePath: req.body.picturePath,
   };
-
-  Profile
-    .findOrCreate({where: profileData})
-    .spread(function(profile, created) {
-      // console.log(profile.get({
-      //   plain: true
-      // }))
-    // console.log('Created: ', created);
-      res.status(201).json(profile.dataValues);
-    });
+  User.findOne({ where: { facebookId: facebookId } }).then(function (user) {
+    Profile.findOrCreate({ where: { userId: user.get('id') } })
+      .then(function (profile) {
+        profile[0].update({ profileData });
+      }).then(function (profile) {
+        res.status(201).json(profile.dataValues);
+      });
+  });
 };
 
-module.exports.fetchOne = function(req, res) {
-  
+module.exports.fetchOne = function (req, res) {
   res.status(200).end();
 };
-
