cb28c9257abb2d5a6d209ec8ad105bf62be3211c
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-08 18:16:59 -0700

    Fix styling

--- controllers/fitnessController.js
+++ controllers/fitnessController.js
@@ -1,25 +1,26 @@
 var Fitness = require('../models/fitness.js');
-var User =  require('../models/user.js');
+var User = require('../models/user.js');
 
-module.exports.saveFitbit = function(fitnessData, facebookId) {
-  User.findOne({where: {facebookId: facebookId}}).then(function(user) {
-    console.log(user.get('id'));
-    Fitness.findOrCreate({ where: {userId: user.get('id')}
-      }).then(function(fitness) {
+module.exports.saveFitbit = function (fitnessData, facebookId) {
+  User.findOne({ where: { facebookId: facebookId } }).then(function (user) {
+    console.log(user);
+    Fitness.findOrCreate({ where: { userId: user.get('id') } })
+      .then(function (fitness) {
+        console.log(fitness);
         fitness[0].update({
           steps: fitnessData.steps,
           restingHeartRate: fitnessData.restingHeartRate,
           sedentaryMinutes: fitnessData.sedentaryMinutes,
           lightlyActiveMinutes: fitnessData.lightlyActiveMinutes,
           fairlyActiveMinutes: fitnessData.fairlyActiveMinutes,
-          veryActiveMinutes: fitnessData.veryActiveMinutes
+          veryActiveMinutes: fitnessData.veryActiveMinutes,
         });
       });
-    });
+  });
 };
-//Logic to refresh fitness data
-// fitbitController.refreshToken(token, id, function(error, access){
-//   fitbitController.fetchFitbitData(access, function(error, data){
-//     fitnessController.saveFitbit(data, id);
-//   });
-// });
+//  Logic to refresh fitness data
+//  fitbitController.refreshToken(token, id, function(error, access){
+//     fitbitController.fetchFitbitData(access, function(error, data){
+//      fitnessController.saveFitbit(data, id);
+//     });
+//  });
