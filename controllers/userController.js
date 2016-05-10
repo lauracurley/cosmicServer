f82682668c360ead9a97d026b4a24575f180f7b3
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-10 12:03:31 -0700

    Implement serveUsers to serve up unmatched userids

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 var User = require('../models/user.js');
-
+var Match = require('../models/match.js');
 
 module.exports.saveOne = function (req, res) {
   var userData = {
@@ -37,5 +37,29 @@ module.exports.isAuthed = function (req, res) {
     });
 };
 
-
+module.exports.serveUsers = function (req, res) {
+  var facebookId = req.body.facebookId;
+  var gender = req.body.gender;
+  User.findOne({ where: { facebookId: facebookId } }).then(function (user) {
+    var userId = user.get('id');
+      Match.findAll({
+        where: { likerUserId : userId },
+        attributes: ['likedUserId', 'matchedIds'],
+      }).then(function (likedMatches) {
+        Match.findAll({
+          where: { likedUserId: userId },
+          attributes: ['likerUserId', 'matchedIds'],
+        }).then(function (likerMatches) {
+          console.log(likedMatches);
+          console.log(likerMatches);
+          var matchedIds = [1, 2, 3, 4, 5];
+          User.findAll({
+            where: { Id: { $notIn: matchedIds } },
+            attributes: ['id','firstName'],
+          }).then(function (userData) {             
+          });
+        });
+      });
+  });
+};
 
