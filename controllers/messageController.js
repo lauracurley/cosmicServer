8c06f2485fd9d97f306d5083ce71a8f2492d2039
Haoming Huang <Hao@Haomings-MacBook-Pro-2.local>
2016-05-09 19:59:39 -0700

    (feat) Complete save one message to database

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,20 +1,31 @@
 var Message = require('../models/message.js');
+var User = require('../models/user.js');
 
 module.exports.saveOne = function (req, res) {
   var messageData = {
-    fromUserId: req.body.fromUserId,
+    fromUserFacebookId: req.body.fromUserFacebookId, 
     toUserId: req.body.toUserId,
     text: req.body.text,
     timestamp: req.body.timestamp,
   };
-  Message
-    .findOrCreate({ where: messageData })
-    .spread(function (message) {
+
+  User.findOne({ where: { facebookId: messageData.fromUserFacebookId } }).then(function (fromUser) {
+    var _fromUserId = fromUser.get('id');
+    var _updatedMessageData = {
+      fromUserId: _fromUserId,
+      toUserId: req.body.toUserId,
+      text: req.body.text,
+      timestamp: req.body.timestamp,
+    };
+
+    Message.create(_updatedMessageData).then(function(message) {
       res.status(201).json(message.dataValues);
-    });
+    }) 
+  });
 };
 
 module.exports.fetchAll = function (req, res) {
   res.status(200).end();
 };
+
 
