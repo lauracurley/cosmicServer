c446d63dbee9fdf0c9ed122e41de5961f5d1dc62
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-10 19:04:57 -0700

    Refactor to es6

--- models/wallet.js
+++ models/wallet.js
@@ -1,33 +1,21 @@
-var Sequelize = require('sequelize');
-var db = require('../database/db.js');
+const Sequelize = require('sequelize');
+const db = require('../database/db.js');
 
-var Wallet = db.define('wallet',
+const Wallet = db.define('wallet',
   {
     id: {
       type: Sequelize.UUID,
       defaultValue: Sequelize.UUIDV1,
-      primaryKey: true
-    },
-    userId: {
-      type: Sequelize.INTEGER,
-      references: {
-        model: 'users',
-        key: 'id',
-      },
-      name: 'userId',
-      field: 'user_id',
+      primaryKey: true,
     },
     steps: {
       type: Sequelize.INTEGER,
-      field: 'steps'
+      field: 'steps',
     },
-  }, 
+  },
   {
-    freezeTableName: true // Model tableName will be the same as the model name
+    freezeTableName: true, // Model tableName will be the same as the model name
   }
 );
 
-
 module.exports = Wallet;
-
-
