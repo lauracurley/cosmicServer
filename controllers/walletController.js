c9eb1a25a70e516897aa9f3c524ed39370c29be5
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-10 19:01:26 -0700

    Refactor to es6

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -2,15 +2,16 @@ var Wallet = require('../models/wallet.js');
 var Profile = require('../models/profile.js');
 var User = require('../models/user.js');
 
-module.exports.spendSteps = function (req, res) {
+module.exports.spendSteps = (req, res) => {
+
   var facebookId = req.body.facebookId;
   var likedId = req.body.likedId;
-  User.findOne({ where: { facebookId: facebookId } }).then(function (user) {
-    Wallet.findOne({ where: { userId: user.get('id') } }).then(function (wallet) {
-      Profile.findOne({ where: { id: likedId } }).then(function (likedProfile) {
+  User.findOne({ where: { facebookId: facebookId } }).then((user) => {
+    Wallet.findOne({ where: { userId: user.get('id') } }).then((wallet) => {
+      Profile.findOne({ where: { userId: likedId } }).then((likedProfile) => {
         // Handle cases where wallet is less than likedUser steps
         var newSteps = wallet.get('steps') - likedProfile.get('steps');
-        Wallet.update({ steps: newSteps });
+        wallet.update({ steps: newSteps });
         res.status(201).send(newSteps);
       });
     });
