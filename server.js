fb96c8351f85572b52c6542d604e7a840d7ac903
Laura Curley <info@lauracurley.com>
2016-05-11 19:59:04 -0700

    Show an index page to indicate site has been deployed

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 
-require('./database/index.js')(); //require and invoke immediately to connect DB
+require('./database/index.js')(); // require and invoke immediately to connect DB
 
 var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 8000;
 
-require('./utilities/middleware.js')(app, express); //config app with required middleware
-require('./router.js')(app, express); //define routes for app
+require('./utilities/middleware.js')(app, express); // config app with required middleware
+require('./router.js')(app, express); // define routes for app
+
+app.use('/', express.static(__dirname));
 
 app.listen(port, (err) => {
   if (err) {
     return console.error('Error listening on port ' + port, err);
   }
-  console.log('App is listening on port '+ port); //start listening for requests on port 8000
+  console.log('App is listening on port ' + port); // start listening for requests on port 8000
 });
 
-module.exports = app; //export app for testing and flexibility
+module.exports = app; // export app for testing and flexibility
 
