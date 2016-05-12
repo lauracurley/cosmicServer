23120550e5c40d89b3337db50dc971382fcc80b9
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-11 17:23:12 -0700

    Refactor to es6 and update function serverUsers to serve data in array of objects
    The returned data now includes restingHeartRate

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
+"use strict";
 const User = require('../models/user.js');
 const Match = require('../models/match.js');
 const Profile = require('../models/profile.js');
+const Fitness = require('../models/fitness.js');
 const Sequelize = require('sequelize');
 
 module.exports.saveOne = (req, res) => {
@@ -28,62 +30,66 @@ module.exports.fetchAll = (req, res) => {
 
 // If a refresh token is saved to the database the user is authorized
 module.exports.isAuthed = (req, res) => {
-  var facebookId = req.body.facebookId;
+  const facebookId = req.query.facebookId;
   User.findOne({ where: { facebookId: facebookId } })
     .then((user) => {
       if (user && user.get('fitbitToken')) {
-        res.status(201).json(true);
+        res.status(201).json({ isAuthed: true });
       } else {
-        res.status(201).json(false);
+        res.status(201).json({ isAuthed: false });
       }
     });
 };
 
 module.exports.serveUsers = (req, res) => {
-  var facebookId = req.body.facebookId;
-  var gender = req.body.gender;
-  var targetGender;
+  const facebookId = req.query.facebookId;
+  const gender = req.query.gender;
+  let targetGender;
   if (gender === 'female') {
     targetGender = 'male';
   } else if (gender === 'male') {
     targetGender = 'female';
   }
   User.findOne({ where: { facebookId: facebookId } }).then((user) => {
-    var userId = user.get('id');
-      Match.findAll({
-        where: Sequelize.or(
-          { likedUserId: userId },
-          { likerUserId: userId }),
-        attributes: ['likedUserId', 'likerUserId'],
-      }).then((matches) => {
-          // var matchedIds = [1, 2, 3, 4, 5];
-        var matchedIds = matches.map((match) => {
-          if (match.get('likedUserId') === userId) {
-            return match.get('likerUserId');
-          } else if (match.get('likerUserId') === userId) {
-            return match.get('likedUserId');
-          }
-        });
-        User.findAll({
-          where: { id: { $notIn: matchedIds } },
-          include: [
-            {
-              model: Profile,
-              where: { gender: targetGender },
-            },
-          ],
-        }).then((userData) => {
-          const userQueue = userData.map((user) => {
-            return ([
-              user.get('firstName'),
-              user.profile.get('picturePath'),
-              user.profile.get('age'),
-              user.profile.get('steps'),
-            ]);
-          });
-          res.status(200).json(userQueue);
-        });
+    const userId = user.get('id');
+    Match.findAll({
+      where: Sequelize.or(
+        { likedUserId: userId },
+        { likerUserId: userId }),
+      attributes: ['likedUserId', 'likerUserId'],
+    }).then((matches) => {
+      const matchedIds = matches.map((match) => {
+        if (match.get('likedUserId') === userId) {
+          return match.get('likerUserId');
+        } else if (match.get('likerUserId') === userId) {
+          return match.get('likedUserId');
+        }
+        return null;
       });
+      User.findAll({
+        where: { id: { $notIn: matchedIds } },
+        include: [
+          {
+            model: Profile,
+            where: { gender: targetGender },
+          },
+          {
+            model: Fitness, 
+          },
+        ],
+      }).then((usersData) => {
+        const userQueue = usersData.map((userData) => {
+          return ({
+            firstName: userData.get('firstName'),
+            picturePath: userData.profile.get('picturePath'),
+            age: userData.profile.get('age'),
+            steps: userData.profile.get('steps'),
+            restingHeartRate: userData.fitness.get('restingHeartRate'),
+          });
+        });
+        res.status(200).json({ userQueue: userQueue });
+      });
+    });
   });
 };
 
