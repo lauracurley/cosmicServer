a8e04701e4e6d764bff21273c3c7b73062db4e4c
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-12 20:39:50 -0700

    Update spendSteps, and implement updateWallet
    spendSteps is now a utility function to handle transaction on matchRequest
    updateWallet is a utility function that will update the wallet based on todays fitness data

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -1,19 +1,55 @@
-var Wallet = require('../models/wallet.js');
-var Profile = require('../models/profile.js');
-var User = require('../models/user.js');
+const Wallet = require('../models/wallet.js');
+const Profile = require('../models/profile.js');
+const User = require('../models/user.js');
+const Fitness = require('../models/fitness.js');
 
-module.exports.spendSteps = (req, res) => {
+module.exports.fetchSteps = (req, res) => {
+  const facebookId = req.query.facebookId;
+  module.exports.updateWallet(facebookId, () => {
+    User.findOne({
+      where: { facebookId },
+      include: [
+        {
+          model: Wallet,
+        },
+      ] }).then(user => {
+        if (user) {
+          res.json(user.wallet.get('steps'));
+        } else {
+          res.status(504).send();
+        }
+      });
+  });
+};
 
-  var facebookId = req.body.facebookId;
-  var likedId = req.body.likedId;
-  User.findOne({ where: { facebookId: facebookId } }).then((user) => {
+module.exports.spendSteps = (facebookId, likedId, callback) => {
+  User.findOne({ where: { facebookId } }).then((user) => {
     Wallet.findOne({ where: { userId: user.get('id') } }).then((wallet) => {
       Profile.findOne({ where: { userId: likedId } }).then((likedProfile) => {
         // Handle cases where wallet is less than likedUser steps
-        var newSteps = wallet.get('steps') - likedProfile.get('steps');
-        wallet.update({ steps: newSteps });
-        res.status(201).send(newSteps);
+        const newSteps = wallet.get('steps') - likedProfile.get('steps');
+        wallet.update({ steps: newSteps }).then(steps => {
+          callback(steps);
+        });
       });
     });
   });
+};
+
+module.exports.updateWallet = (facebookId, callback) => {
+  User.findOne({
+    where: { facebookId },
+    include: [
+      {
+        model: Wallet,
+      },
+      {
+        model: Fitness,
+      },
+    ] }).then(user => {
+      const steps = user.fitness.get('steps');
+      user.wallet.get('steps');
+      user.wallet.set('steps', steps);
+      callback(user.wallet.get('steps'));
+    });
 };
