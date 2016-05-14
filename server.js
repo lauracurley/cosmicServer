96011e985cccf23c7a3574e0ce979d27c8c90981
haomingh <haomingh@users.noreply.github.com>
2016-05-14 16:45:33 -0700

    (log) Cleanup zombie code

--- server.js
+++ server.js
@@ -26,27 +26,21 @@ io.on('connection', (socket) => {
 
     connected[facebookId] = socket.id;
     console.log('Connected: ', connected);
-    // console.log('Connected FacebookId: ', facebookId);
-    // console.log('Connected SocketId: ', socket.id);
     console.log('Connected Rooms: ', socket.rooms);
   });
  
   socket.on('notifyOtherUserToFetchLast', function(facebookIds) {
-    // If the other person is connected,
-    // if (connected.hasOwnProperty('10206426211234693')) { //CHANGE THIS TO A VARIABLE
-    // }
     console.log('Connected on fetch: ', connected);
     console.log('Connected Rooms on fetch: ', socket.rooms);
     console.log('Tell this person to fetch: ', facebookIds.toUserFacebookId, ' ', connected[facebookIds.toUserFacebookId]);
     
     var socketToFetch = connected[facebookIds.toUserFacebookId];
     // var socketToFetch = connected['10206426211234693'];
+
+    // If the other person is connected...
     if (socketToFetch !== undefined) {
       io.to(socketToFetch).emit('fetchLast');
     }
-    // socket.emit('hello');
-    // socket.broadcast.to('10206426211234693').emit('hello');
-    // io.sockets.in('10206426211234693').emit('hello');
 
   }); 
 
